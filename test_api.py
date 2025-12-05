300ef61c6a5ba7ac7a93c0d6a94af34a402bf655
Ludovic Chaumaz <54986137+ludovicchmz@users.noreply.github.com>
2025-12-05 14:28:36 +0100

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -8,7 +8,7 @@ clients = pd.read_csv("for_unit_testing.csv")
 #Test 1 : On a bien une erreur si "clients" n'est pas un DataFrame
 def test_strange_type():
     # Arrange
-    id = 228569
+    id = 100001
     clients = [1,2,3,4]
     
     #Assert
@@ -18,20 +18,20 @@ def test_strange_type():
 #Test 2 : On a bien une erreur si l'identifiant du client n'est pas connu
 def test_strange_id():
     # Arrange
-    id = 100001
+    id = 000000
     
     #Assert
     with pytest.raises(ValueError):
         outcome = API.get_client_feats(clients, id)
 
-#Test 3 : La valeur du montant du crédit du client 110822 est bien celle indiquée 
+#Test 3 : La valeur du montant du crédit du client 100001 est bien celle indiquée 
 #dans le tableau
-def test_value_credit_228569():
+def test_value_credit_100001():
     # Arrange
-    id = 228569
+    id = 100001
     
     #Act
     outcome = API.get_client_feats(clients, id)
     
     #Assert
-    assert outcome[0][5] == 248760 
+    assert outcome[0][5] == 568800
